f8193f5a0fa861decca40c2a5598e2330ade3ea3
Petar Plackov <perap88@gmail.com>
2024-04-26 22:58:32 +0200

    homepage desktop in progress

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,9 @@
-
-
-      /* $(document).ready(function(){
-
-        $('.icon').on('click', function(e) {
-          e.preventDefault();
-          $(this).closest('.single-item').find('.tooltip-content').addClass("open");
-          console.log("test");
-           $('body').addClass("fixed-class");
-           $('body').addClass("overlay");
-        });
-    
-         $('.tooltip').on('click', function(e) {
-           e.preventDefault();
-           $(this).closest('.single-item').find('.tooltip-content').addClass("open");
-           $('body').addClass("fixed-class");
-           $('body').addClass("overlay");
-         });
-    
-         $('.exit').on('click', function(e) {
-           e.preventDefault();
-           $('.tooltip-content').removeClass("open");
-           $('body').removeClass("fixed-class");
-           $('body').removeClass("overlay");
-         });
-      });  /*
+// Header Sticky
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('.site-header');
+  if (window.scrollY > 1) {
+    header.classList.add('site-header__sticky');
+  } else {
+    header.classList.remove('site-header__sticky');
+  }
+});
