df1418431c8669678109c8c04da3ffeb6aa29096
Petar Plackov <perap88@gmail.com>
2024-04-29 15:10:22 +0200

    slick slider setup

--- app/js/site/sliders.js
+++ app/js/site/sliders.js
@@ -1,19 +1,14 @@
 export function initSlider() {
-    console.log("log");
-    document.addEventListener("DOMContentLoaded", function() {
-        console.log("log 2");
-        const slider = document.querySelector('.banner-slider__wrapper');
-        const slickOptions = {
-            slidesToShow: 1,
+    $(document).ready(function(){
+        $('.banner-slider__wrapper').slick({
+            slislidesToShow: 1,
             slidesToScroll: 1,
-            autoplay: true,
+            autoplay: false,
             autoplaySpeed: 2000,
-            dots: true
-        };
-        
-        if (slider) {
-            new Slick(slider, slickOptions);
-            console.log("log 3");
-        }
+            arrows: true,
+            dots: false,
+            prevArrow: $('.custom-prev-arrow'),
+            nextArrow: $('.custom-next-arrow')
+        });
     });
 }
