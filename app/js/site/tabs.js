de25c23a2f00e09dd74ec753c467d1241d0a9daa
Petar Plackov <perap88@gmail.com>
2024-04-29 23:20:00 +0200

    404, privacy in progress

--- app/js/site/tabs.js
+++ app/js/site/tabs.js
@@ -1,28 +1,4 @@
-export function initTabs() {
-    // document.querySelectorAll('.four-column-filter__box button').forEach(function(btn) {
-    //     btn.addEventListener('click', function() {
-    //         var attr = this.getAttribute('data-li');
-    
-    //         document.querySelectorAll('.four-column-filter__box button').forEach(function(btn) {
-    //             btn.classList.remove('active');
-    //         });
-    //         this.classList.add('active');
-    
-    //         document.querySelectorAll('.four-column__slider-box').forEach(function(item) {
-    //             item.style.display = 'none';
-    //         });
-    
-    //         if (attr && (attr == "all" || attr == "hatchback" || attr == "sportback" || attr == "limousine" || attr == "suv" || attr == "cabrio")) {
-    //             document.querySelectorAll('.' + attr).forEach(function(item) {
-    //                 item.style.display = 'block';
-    //             });
-    //         } else {
-    //             document.querySelectorAll('.four-column__slider-box').forEach(function(item) {
-    //                 item.style.display = 'block';
-    //             });
-    //         }
-    //     });
-    // }); 
+export function initTabs() { 
     document.querySelectorAll('.four-column-filter__box button').forEach(function(btn) {
         btn.addEventListener('click', function() {
             var attr = this.getAttribute('data-li');
